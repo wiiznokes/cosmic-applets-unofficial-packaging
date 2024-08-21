66ec2a441abae1fe201ea359a28b5288cf9f9ca7
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-08-21 04:40:28 +0200

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -xe
 
-
+PACKAGE=cosmic-ext-applet-clipboard-manager
+REPO=https://github.com/wiiznokes/clipboard-manager
 
 git clone --recurse-submodules https://github.com/wiiznokes/cosmic-applets-unofficial-packaging.git
 cp cosmic-applets-unofficial-packaging/rpms/$PACKAGE/* .
 cp cosmic-applets-unofficial-packaging/scripts/srpm.sh .
 
+./srpm.sh
